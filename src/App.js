521115d1658f0acb2144c2a792b6333f3c7cf4f0
Ivonne Hernandez <ivonne702@gmail.com>
2022-03-16 19:37:06 -0600

    Add Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import { getCurrentWeeksBestsellers } from './apiCalls';
+import Dropdown from './Components/Dropdown/Dropdown';
 import Table from './Components/Table/Table.js';
-
 import './App.css';
+
 
 const App = () => {
   const [lists, setLists] = useState([]);
   const [date, setDate] = useState('');
+  const [selectedList, setSelectedList] = useState('All');
 
   useEffect(() => {
     getCurrentWeeksBestsellers()
@@ -19,6 +21,10 @@ const App = () => {
   return (
     <div className="App">
       <h1>New York Times Bestsellers List</h1>
+      <Dropdown 
+        setSelectedList={setSelectedList} 
+        options={lists.map(list => list.list_name)}
+        selectedList={selectedList} />
       <Table lists={lists} date={date} />
     </div>
   )
---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,43 @@
+import './Dropdown.css';
+
+const Dropdown = ({setSelectedList, selectedList, options}) => {
+  const renderListOptions = () => {
+    const allOptions = [
+      <option
+        className='list-option' 
+        name="All" 
+        value="All"
+        key="All">
+          All
+      </option>
+    ];
+    const listOptions = options.map(option => {
+      return (
+        <option 
+          className='list-option' 
+          name={option} 
+          value={option}
+          key={option} >
+          {option}
+        </option>
+      );
+    });
+    allOptions.push(...listOptions);
+    return allOptions;
+  }
+  return (
+    <div className='Dropdown'>
+      <label htmlFor='dropdown'>
+        Select Bestseller List
+      </label>
+      <select
+        id='dropdown'
+        value={selectedList}
+        onChange={(event) => setSelectedList(event.target.value)}>
+        {renderListOptions()}
+      </select>
+    </div>
+  )
+}
+
+export default Dropdown;
